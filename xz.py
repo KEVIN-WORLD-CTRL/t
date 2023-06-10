67fe389030b8e93d2bfd277f3fa3a03ddd09f245
KEVIN-WORLD-CTRL <133050888+KEVIN-WORLD-CTRL@users.noreply.github.com>
2023-06-11 00:46:18 +0600

    Add files via upload

---
+++ xz.py
@@ -0,0 +1,16 @@
+import os, platform
+ 
+try:
+        import requests
+        
+        os.system("git pull")
+        
+except:
+        os.system('pip2 install requests')
+import requests
+bit = platform.architecture()[0]
+if bit == "64bit":
+        from xk 
+elif bit == "32bit":
+        from xk 
+        
